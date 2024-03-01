859fff8f6c02530f6cdd645c3542d983bc0fffc8
ebokyung <dlqhrud303@naver.com>
2024-03-01 20:00:50 +0900

    [type-challenges] 0301

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,3 @@
-type Concat<T, U> = any
+type Tuple = readonly unknown[] // const tuple = [1] as const -> readonly!!
+
+type Concat<T extends Tuple, U extends Tuple> = [...T, ...U]
--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,9 @@
-type Includes<T extends readonly any[], U> = any
+// import type { Equal } from '@type-challenges/utils' 참고
+type IsEqual<X, Y> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? true : false
+
+type Includes<T extends readonly unknown[], U> =
+  T extends [infer First, ...infer Rest]
+    ? IsEqual<First, U> extends true ? true : Includes<Rest, U>
+    : false
